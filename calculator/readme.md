61c4efe537cff210b4e77b5f36d33e6844f03b2e
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-09 22:22:18 +0200

    [UPD] Millestone 1

---
+++ calculator/readme.md
@@ -0,0 +1,11 @@
+MILESTONE 1
+Aggiungere event listener a tutti i numeri (0-9): quando si clicca su un numero, concatenarlo al numero visualizzato in alto
+
+MILESTONE 2
+aggiungere event listener a tutti gli operatori: quando si clicca su un operatore, salvare l'operatore cliccato e il primo operando, quindi resettare il numero in alto
+
+MILESTONE 3
+aggiungere event listener al pulsante "=": quando si clicca su =, salvare il secondo operando, effetuare il calcolo corretto in base all'operatore selezionato e visualizzare il risultato in alto
+
+MILESTONE 4
+pulsante canc: cliccando il pulsante si resetta il calcolo
--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 // reset js 
-'use strict'
+"use strict";
+
+// controllo collegamento 
 
 console.log('ciao mondo');
+
+// variabile per tenere traccia del numero attualmente visualizzato
+let currentNumber = ""; 
+
+// seleziona tutti i pulsanti numerici 
+const numbers = document.querySelectorAll(".numbers button");
+
+// seleziona l'elemento risultato
+const result = document.getElementById("result");
+
+// aggiunta delll'event listener a tutti i numeri utilizzando un ciclo for
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", selectNumber);
+}
+
+// funzione per gestire la selezione di un numero
+function selectNumber() {
+    const numberValue = this.innerText;
+
+    // concateniamo il numero cliccato a quello attuale
+    currentNumber += numberValue; 
+
+    // aggiornamento del risultato risultato visualizzato
+    result.innerText = currentNumber; 
+}
